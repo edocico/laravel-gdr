f5850b07ee945bc486ab846aa9c3c086679dd198
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-15 18:00:06 +0100

    ADD content to index.blade.php

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello World</h1>
+    <div class="container">
+        <div class="row row-gap-5">
+            @foreach($characters as $character)
+                <div class="col-3">
+                    <div class="card h-100" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$character->name}}</h5>
+
+                          <p class="card-text">Bio: {{$character->bio}}</p>
+                          <p class="card-text">Defense: {{$character->defense}}</p>
+                          <p class="card-text">Speed: {{$character->speed}}</p>
+                          <p class="card-text">Hp: {{$character->hp}}</p>
+                        </div>
+                      </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
