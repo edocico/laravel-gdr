4fd637edb2c5f7f4eafeecdcdc000e93dbf3aa56
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-20 16:56:28 +0100

    ADD header-menu

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <ul class="nav nav-underline">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('characters.index') }}">comics</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
