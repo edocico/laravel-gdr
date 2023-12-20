56253245eb7cefc416c142236fb7c8d61df5dc1b
sabri <sabrinatwitter6@gmail.com>
2023-12-20 15:44:44 +0100

    edit route

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/', [PageController::class, 'index'])
+    ->name('home');
